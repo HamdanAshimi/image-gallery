083239405a073b7f91dddd5c40778e4a70687647
HamdanAshimi <ashimiademola4@gmail.com>
2026-08-27 05:49:53 +0100

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import Search from "./components/Search";
 function App() {
   return (
     // Global Container
-    <div class="flex items-center justify-center min-h-screen bg-cyan-50">
+    <div className="flex items-center justify-center min-h-screen bg-cyan-50">
       {/* Card Container */}
-      <div class="bg-white p-6 m-3 space-y-10 shadow-2xl rounded-3xl md:p-40">
+      <div className="bg-white p-6 m-3 space-y-10 shadow-2xl rounded-3xl md:p-40">
         <Menu />
         <Search />
         <GalleryGrid />

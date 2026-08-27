702fc306f78d0875b09d869e59c34b6b00fc6823
HamdanAshimi <ashimiademola4@gmail.com>
2026-08-27 05:36:13 +0100

    Add menu search and gallery components to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import "./App.css";
+import GalleryGrid from "./components/GalleryGrid";
+import Menu from "./components/Menu";
+import Search from "./components/Search";
 
 function App() {
-  return <></>;
+  return (
+    // Global Container
+    <div class="flex items-center justify-center min-h-screen bg-cyan-50">
+      {/* Card Container */}
+      <div class="bg-white p-6 m-3 space-y-10 shadow-2xl rounded-3xl md:p-40">
+        <Menu />
+        <Search />
+        <GalleryGrid />
+      </div>
+    </div>
+  );
 }
 
 export default App;
